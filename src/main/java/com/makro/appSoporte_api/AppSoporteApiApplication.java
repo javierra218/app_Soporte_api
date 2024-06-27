1e343197c7e4830f549cbef9d1ee91bb5ca3717b
Javier Rojas <javierra@unicauca.edu.co>
2024-06-27 11:25:08 -0500

    Initial commit

---
+++ src/main/java/com/makro/appSoporte_api/AppSoporteApiApplication.java
@@ -0,0 +1,13 @@
+package com.makro.appSoporte_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSoporteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSoporteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makro/appSoporte_api/AppSoporteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makro.appSoporte_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSoporteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
